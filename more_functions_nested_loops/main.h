185b06e5c6bafcc58b6513dbf26df0e244ef218b
Your Name <you@example.com>
2026-02-28 22:17:25 +0000

    Task 8: function that prints a square

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,7 +3,6 @@
 
 int _putchar(char c);
 int _isupper(int c);
-
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -11,5 +10,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+
 void print_square(int size);
 #endif
