bf22577386026e7538d72e8a76555cad65d71599
aseelmi-lab <aseel.mashall@gmail.com>
2026-03-08 08:05:08 +0000

    Task 3: Implement _strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+	int found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0)
+			return (count);
+	}
+	return (count);
+}
