b888bd838583587694dbe31d1b145c9725c13570
aseelmi-lab <aseel.mashall@gmail.com>
2026-03-08 08:15:15 +0000

    Task 5: Implement _strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to search in
+ * @needle: the substring to look for
+ *
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
