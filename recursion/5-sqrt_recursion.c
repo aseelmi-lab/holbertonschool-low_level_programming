9bc8c3a91906f4a10ab3f540bb2dfaeae66737c6
aseelmi-lab <aseel.mashall@gmail.com>
2026-03-08 19:37:14 +0000

    Task 5: find natural square root using a helper recursive function

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _actual_sqrt(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_actual_sqrt(n, 0));
+}
+
+/**
+ * _actual_sqrt - recurses to find the natural square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int _actual_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_actual_sqrt(n, i + 1));
+}
