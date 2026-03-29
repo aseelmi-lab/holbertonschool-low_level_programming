ba7552e728028c2066f31531cf51c6fb414d6ec0
aseelmi-lab <your_email@example.com>
2026-03-29 07:01:28 +0000

    Fix Betty style: split long lines in 4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid
+ * free_grid - frees a 2 dimensional grid previously
+ * created by your alloc_grid function.
  * @grid: 2D grid to free
  * @height: height of the grid
  *
@@ -12,16 +13,13 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	/* التأكد من أن المصفوفة ليست فارغة أصلاً */
 	if (grid == NULL || height <= 0)
 		return;
 
-	/* 1. تحرير كل صف منفرداً */
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
 
-	/* 2. تحرير المصفوفة الرئيسية التي تحتوي على مؤشرات الصفوف */
 	free(grid);
 }
