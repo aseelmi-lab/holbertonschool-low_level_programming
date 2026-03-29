530d764685c2366171a2a22c9d1e496552705016
aseelmi-lab <your_email@example.com>
2026-03-29 06:54:34 +0000

    Fix Betty style: line over 80 characters

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,27 +16,25 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	/* 1. حجز مصفوفة المؤشرات (للصفوف) */
+	/* حجز مصفوفة المؤشرات للصفوف */
 	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 		return (NULL);
 
-	/* 2. حجز مصفوفة الأرقام لكل صف */
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 
-		/* إذا فشل الحجز، نقوم بتحرير كل ما سبق حجزُه */
 		if (grid[i] == NULL)
 		{
+			/* إذا فشل الحجز، نقوم بالتحرير */
 			for (; i >= 0; i--)
 				free(grid[i]);
 			free(grid);
 			return (NULL);
 		}
 
-		/* 3. تهيئة العناصر بـ 0 */
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
