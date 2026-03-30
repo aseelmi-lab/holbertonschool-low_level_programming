bc477999d89957ddf2fe3784c388bdfb2f967980
aseelmi-lab <aseel@example.com>
2026-03-30 19:26:18 +0000

    Final absolute fix for valgrind and logic

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
