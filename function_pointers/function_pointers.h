5bdc0bcd298b9a0d5b23ee55dd968bf009788290
aseelmi-lab <aseel@example.com>
2026-04-05 06:58:32 +0000

    Task 1 and 2: Implement array_iterator and int_index

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,8 +2,17 @@
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * file: function_pointers.h
+ * desc: Header file containing prototypes for all functions
+ * used in the 0x0e-function_pointers project.
+ */
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
