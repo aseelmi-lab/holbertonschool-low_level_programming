f67ece114a1a9c820dd811ff3ff1e50c22c69cd3
aseelmi-lab <aseel.mashall@gmail.com>
2026-04-06 07:15:34 +0000

    Task 1: Implement print_numbers

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,6 +3,16 @@
 
 #include <stdarg.h>
 
+/* Task 0 */
 int sum_them_all(const unsigned int n, ...);
+
+/* Task 1 */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* Task 2 (للمهمة القادمة) */
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/* Task 3 (للمهمة الأخيرة) */
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
