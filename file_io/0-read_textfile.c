e65bde8e4dd64c1a6458c9905ab8e8190fb1f355
aseelmi-lab <aseel.mashall@gmail.com>
2026-04-22 10:08:39 +0000

    style: fix Betty line length warning

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to POSIX stdout.
  * @filename: pointer to the name of the file.
  * @letters: number of letters it should read and print.
  *
