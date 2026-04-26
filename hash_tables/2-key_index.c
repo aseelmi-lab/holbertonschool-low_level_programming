fec81d5268e1084fa75945821c53930c34d9e58a
aseelmi-lab <aseel.mashall@gmail.com>
2026-04-26 16:25:39 +0000

    style: fix Betty line length in Task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -11,9 +11,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
 
-	/* 1. حساب الـ hash value باستخدام دالة djb2 */
 	hash = hash_djb2(key);
 
-	/* 2. تحويل الرقم الكبير إلى اندكس مناسب لحجم المصفوفة */
 	return (hash % size);
 }
